b8017ab076f953317f406da042165e6be4f062ad
093C467E3 <ignacio.s.tamarit@gmail.com>
2024-05-08 17:48:42 +0200

    added dates (position 0) to the text array and corrected minor bugs

--- textual_analysis/scrapper_v0.py
+++ textual_analysis/scrapper_v0.py
@@ -14,11 +14,15 @@ from tools_scrapping import *
 
 
 url = 'https://www.coindesk.com/'
-max_time_running = 30
+max_time_running = 600
 
 starting_time = time.time()
 current_time  = time.time()
 time_elapsed  = current_time - starting_time
+
+time_checks = 10
+time_step   = max_time_running/time_checks
+step = 0
 
 non_analyzed_links = set()
 analyzed_links = set()
@@ -26,40 +30,42 @@ analyzed_links = set()
 non_analyzed_links.add(url)
 
 while non_analyzed_links != {} and time_elapsed < max_time_running:
-    
+
     local_url = non_analyzed_links.pop()
     response = requests.get(local_url)
-    
+
     ## Assess that we recieved a response
-    
+
     if not response.ok:
         print('Error recieving a response')
-    
+
     ## Gather and classify the links observed in the response
-    
+
     links = gather_links(response)
     links = add_root(links, url)
     links = drop_external_links(links, url)
-    
+
     for link in links:
         if link in analyzed_links:
             continue
         elif url in link:
             non_analyzed_links.add(link[:-1])
-            
+
     del links
-    
+
     ## Gather and save the content observed within the response
-    
+
     soup = BeautifulSoup(response.content, "html.parser")
-    
+
+    ## Gather textual data
+
     texts = soup.findAll(text=True)
     visible_texts_bis = list(filter(tag_visible, texts)).copy()
     visible_texts_bis = map(remove_special_chars, visible_texts_bis)
     visible_texts = []
     [visible_texts.append(x) for x in visible_texts_bis if x not in visible_texts]
     visible_texts = np.array(visible_texts)
-    
+
     if visible_texts.size == 0:
         continue
 
@@ -67,18 +73,38 @@ while non_analyzed_links != {} and time_elapsed < max_time_running:
     chars = np.fromiter(chars, dtype=int)
     thershold = np.quantile(chars, q = 0.8)
     visible_texts = visible_texts[chars > thershold]
-    
+
+
+    ## Gather date, this is specific for coindesk:
+
+    try:
+        date = soup.find_all("meta", attrs={"property": "article:published_time"})[0]['content']
+    except IndexError:
+        continue
+
+    timestamp = str(string_to_timestamp_coindesk(date))
+
+    visible_texts = np.concatenate(([str(timestamp)], visible_texts))
+
     ## Save data
-    
+
     local_url = local_url.replace('/','-')
     filename = f'Data/{local_url}.npy'
-    
+
     if len(filename) > 50:
         filename = filename[:50]
-    
+
     np.save(filename, visible_texts)
-    
+
     ## updates for the next iteration
     
+    analyzed_links.add(local_url)
+
     current_time  = time.time()
     time_elapsed  = current_time - starting_time
+
+    if time_elapsed > time_step * step:
+        step += 1
+        print(f"Number non analyzed documents: {len(non_analyzed_links)}")
+        print(f"Number analyzed documents: {len(analyzed_links)}")
+        print("="*70)
--- textual_analysis/tools_scrapping.py
+++ textual_analysis/tools_scrapping.py
@@ -15,29 +15,29 @@ def gather_links(response):
     Returns a list of links observed within the response input
     """
     links = []
-    
+
     for link in BeautifulSoup(response.content, parse_only=SoupStrainer('a')):
         try:
             if link.has_attr('href'):
                 links.append(link['href'])
-                
+
         except AttributeError:
             pass
-    
+
     return links
 
 def add_root(links, url):
     for idx, link in enumerate(links):
         if 'http' not in link:
             links[idx] = url + link
-    
+
     return links
 
 def drop_external_links(links, url):
     for idx, link in enumerate(links):
         if url not in link:
             links.remove(link)
-            
+
     return links
 
 def tag_visible(element):
@@ -48,3 +48,27 @@ def tag_visible(element):
     return True
 
 remove_special_chars = lambda string: re.sub(r"[^a-zA-Z0-9]+", ' ', string)
+
+def clean_links(links):
+    clean_links_array = []
+    for link in links:
+        if "quote" in link and "-USD" in link:
+            clean_links_array.append(link.replace("/quote/",""))
+
+    return clean_links_array
+
+def string_to_timestamp_coindesk(date_string):
+    # Define format strings with and without milliseconds
+    format_with_ms = '%Y-%m-%dT%H:%M:%S.%fZ'
+    format_without_ms = '%Y-%m-%dT%H:%M:%SZ'
+    
+    try:
+        # Try parsing with milliseconds
+        date_object = datetime.strptime(date_string, format_with_ms)
+    except ValueError:
+        # If milliseconds are not present, parse without milliseconds
+        date_object = datetime.strptime(date_string, format_without_ms)
+        
+    # Convert datetime object to timestamp
+    timestamp = date_object.timestamp()
+    return timestamp
